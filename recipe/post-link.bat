317d1feef5e8f48b5bda48576961dc3c9c903375
Uwe L. Korn <uwe.korn@quantco.com>
2019-12-08 16:57:30 +0100

    Remove JavaScript part of the package

--- recipe/post-link.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"%PREFIX%\Scripts\python.exe" -m 'jupyter_core.command' serverextension enable jupyterlab_code_formatter --py --sys-prefix > "%PREFIX%\.messages.txt" 2>&1 && if errorlevel 1 exit 1
